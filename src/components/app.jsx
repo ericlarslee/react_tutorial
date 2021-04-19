f2f2bb00b87d027aa601f47ae1e9f4327b3e42b9
Eric Lars Lee <ericlarslee@gmail.com>
2021-04-19 14:56:38 -0500

    added function that allows to go through list of books. When showing current book, displays a rough rendering of a book with title and author printed on the cover

--- src/components/app.jsx
+++ src/components/app.jsx
@@ -1,12 +1,50 @@
 import React, { Component } from 'react';
 import TitleBar from './TitleBar/titleBar';
+import './app.css';
+import BookViewer from './BookViewer/bookViewer';
 
 class App extends Component {
+    constructor(props) {
+        super(props);
+        this.books = [
+            {title: 'For Whom the Bell Tolls', author: 'Ernest Hemingway'},
+            {title:'The Imitation of Christ', author:'Thomas A Kempis'},
+            {title:'In Cod We Trust', author:'Eric Dregni'}
+        ];
+        this.state = {
+            bookNumber: 0
+        }
+    }
+
+    goToNextBook(){
+        let tempBookNumber = this.state.bookNumber;
+        tempBookNumber++;
+        if(tempBookNumber === this.books.length){
+            tempBookNumber = 0;
+            }
+            this.setState({
+                bookNumber: tempBookNumber
+            });
+    }
+
+    goToLastBook(){
+        let tempBookNumber = this.state.bookNumber;
+        tempBookNumber--;
+        if(tempBookNumber < 0){
+            tempBookNumber = this.books.length -1;
+        }
+        this.setState({
+            bookNumber: tempBookNumber
+        });
+    }
+
     state = {  }
     render () {
         return (
             <div className="container-fluid">
                 <TitleBar />
+                <BookViewer book={this.books[this.state.bookNumber]} nextBook={() => this.goToNextBook()}
+                lastBook={() => this.goToLastBook} />
             </div>
         )
     }
